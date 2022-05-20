278ac521fb1e35245826eadb47e3ea679c03c08f
DEV-COM <kwp@tlen.pl>
2022-05-20 14:30:09 +0200

    piatek 14:30

--- src/app/race.service.ts
+++ src/app/race.service.ts
@@ -1,17 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { interval, Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { PonyWithPositionModel } from './models/pony.model';
 
-import { RaceModel } from './models/race.model';
+import { RaceModel,LiveRaceModel } from './models/race.model';
+import { WsService } from './ws.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RaceService {
 
-constructor(private http: HttpClient) {}
+constructor(private http: HttpClient, private wsService: WsService) {}
 
   list(): Observable<Array<RaceModel>> {
     
@@ -32,66 +33,7 @@ constructor(private http: HttpClient) {}
     }
 
     live(raceId:number): Observable<Array<PonyWithPositionModel>>{
-      return interval(1000).pipe(
-        take(101),
-        map(position => [
-          {id: 1,
-            name: 'Superb Runner',
-            color: 'BLUE',
-            position
-          },
-          {
-            id: 2,
-            name: 'Awesome Fridge',
-            color: 'GREEN',
-            position
-          },
-          {
-            id: 3,
-            name: 'Great Bottle',
-            color: 'ORANGE',
-            position
-          },
-          {
-            id: 4,
-            name: 'Little Flower',
-            color: 'YELLOW',
-            position
-          },
-          {
-            id: 5,
-            name: 'Nice Rock',
-            color: 'PURPLE',
-            position
-          }
-        ])
-      );
+      return this.wsService.connect<LiveRaceModel>(`/race/${raceId}`).pipe(map(liveRace => liveRace.ponies));
     }
   }
-    // return of([
-    //   {
-    //     id: 12,
-    //     name: 'Paris',
-    //     ponies: [
-    //       { id: 1, name: 'Gentle Pie', color: 'YELLOW' },
-    //       { id: 2, name: 'Big Soda', color: 'ORANGE' },
-    //       { id: 3, name: 'Gentle Bottle', color: 'PURPLE' },
-    //       { id: 4, name: 'Superb Whiskey', color: 'GREEN' },
-    //       { id: 5, name: 'Fast Rainbow', color: 'BLUE' }
-    //     ],
-    //     startInstant: '2020-02-18T08:02:00Z'
-    //   },
-    //   {
-    //     id: 13,
-    //     name: 'Tokyo',
-    //     ponies: [
-    //       { id: 6, name: 'Fast Rainbow', color: 'BLUE' },
-    //       { id: 7, name: 'Gentle Castle', color: 'GREEN' },
-    //       { id: 8, name: 'Awesome Rock', color: 'PURPLE' },
-    //       { id: 9, name: 'Little Rainbow', color: 'YELLOW' },
-    //       { id: 10, name: 'Great Soda', color: 'ORANGE' }
-    //     ],
-    //     startInstant: '2020-02-18T08:03:00Z'
-    //   }
-    // ]).pipe(delay(500));
  
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://ponyracer.ninja-squad.com'
+  baseUrl: 'https://ponyracer.ninja-squad.com',
+  wsBaseUrl: 'wss://ponyracer.ninja-squad.com'
 
 };
