60e79cc29f667eb31d415b04d7881f020065c8c0
DEV-COM <kwp@tlen.pl>
2022-05-20 10:44:24 +0200

    Piatek 10:40

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,13 @@ import { RacesComponent } from './races/races.component';
 import { RaceComponent } from './race/race.component';
 import { PonyComponent } from './pony/pony.component';
 import { FromNowPipe } from './from-now.pipe';
+import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
+import { ROUTES } from './app.routes';
 
 @NgModule({
-  declarations: [AppComponent, MenuComponent, RacesComponent, RaceComponent, PonyComponent, FromNowPipe],
-  imports: [BrowserModule, HttpClientModule],
+  declarations: [AppComponent, MenuComponent, RacesComponent, RaceComponent, PonyComponent, FromNowPipe, HomeComponent],
+  imports: [BrowserModule, HttpClientModule, RouterModule.forRoot(ROUTES)],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from "@angular/router";
+import { HomeComponent } from "./home/home.component";
+import { RacesComponent } from "./races/races.component";
+
+
+
+export const ROUTES: Routes = [
+    { path: '', component: HomeComponent},
+    { path: 'races',component: RacesComponent}
+];
