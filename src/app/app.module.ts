8e2b2452040883d60c3079d1036451bcc382e904
DEV-COM <kwp@tlen.pl>
2022-05-20 07:38:22 +0200

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { RacesComponent } from './races/races.component';
+
+@NgModule({
+  declarations: [AppComponent, MenuComponent, RacesComponent],
+  imports: [BrowserModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
---
+++ src/app/race.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+import { RaceModel } from './models/race.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RaceService {
+  list(): Array<RaceModel> {
+    return [{ name: 'Lyon' }, { name: 'Los Angeles' }, { name: 'Sydney' }, { name: 'Tokyo' }, { name: 'Casablanca' }];
+  }
+}
