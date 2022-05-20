b57c2042f21ab30842139e05540f6f6ac691e768
DEV-COM <kwp@tlen.pl>
2022-05-20 10:17:23 +0200

    piatek 10:20

--- src/app/pony/pony.component.ts
+++ src/app/pony/pony.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+import { PonyModel } from '../models/pony.model';
 
 @Component({
   selector: 'pr-pony',
   templateUrl: './pony.component.html',
   styleUrls: ['./pony.component.css']
 })
-export class PonyComponent implements OnInit {
+export class PonyComponent {
 
-  constructor() { }
+  @Input() ponyModel!: PonyModel;
+  @Output() readonly ponyClicked = new EventEmitter<PonyModel>();
 
-  ngOnInit(): void {
+  getPonyImageUrl(): string {
+    return `assets/images/pony-${this.ponyModel.color.toLowerCase()}.gif`;
   }
 
+  clicked(): void{
+    this.ponyClicked.emit(this.ponyModel);
+  }
 }
